7b9ea8484d94b4f8e385a37c7bc2aad595b430d9
Виктория <Ovcharka@gmail.com>
2019-01-15 11:24:28 +0300

    готово, есть косяки

--- project/js/open-filter.js
+++ project/js/open-filter.js
@@ -1,21 +1,23 @@
 var $filterLink = $('.filter__toggle'),
-	$filterForm = $('.filter-form');
+	$filterForm = $('.filter-form'),
+	$body = $('body');
 var filterInput = document.querySelectorAll('.input-btn__label');
 
 $filterLink.on('click', function() {
 	$filterForm.addClass('filter-form--show');
-	$filterForm.css('top', $('#some_point').offset().top - 100);
+	$filterForm.css('top', $('#some_point').offset().top);
 	$('html,body').stop().animate({ scrollTop: $('#some_point').offset().top }, 1000);
+
+	$body.addClass('overlay');
 });
 
 function closeFilter(elem) {
 	elem.addEventListener('click', function() {
 		$filterForm.removeClass('filter-form--show');
+		$body.removeClass('overlay');
 	});
 }
 
 for (var i=0; i < filterInput.length-1; i++) {
 	closeFilter(filterInput[i]);
 }
-
-console.log($('#some_point').offset().top);
--- project/js/popup.js
+++ project/js/popup.js
@@ -1,18 +1,23 @@
 var $link = $('.goods__img--active'),
 	$excerpt = $('.excerpt--active'),
 	$cardGoods = $('.big-goods__item'),
-	$cardClose = $(".big-goods__close");
-
+	$cardClose = $(".big-goods__close"),
+	$body = $('body');
 
 function openCardGoods(element, evt) {
 	element.on('click', function(evt) {
 		evt.preventDefault();
 		$cardGoods.addClass('big-goods__item--show');
 
-		var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		$cardGoods.animate({ "top": scrollTop}, 1500);
+		if ($(window).width() >= 1200) {
+			$cardGoods.animate({ "top": '20%'}, 1500);
+		} else {
+			var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			$cardGoods.animate({ "top": scrollTop}, 1500);
+		}
 
 		$cardGoods.addClass('active'); 
+		$body.addClass('overlay');
 	});
 }
 
@@ -21,6 +26,7 @@ openCardGoods($excerpt);
 
 $cardClose.on('click', function() {
 	$cardGoods.removeClass('big-goods__item--show');
-	$cardGoods.removeClass('active'); 
+	$cardGoods.removeClass('active');
+	$body.removeClass('overlay'); 
 	$cardGoods.css('top', '3000px');
 });
